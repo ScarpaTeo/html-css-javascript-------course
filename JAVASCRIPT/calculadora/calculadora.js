5db33ce0a1fa81ad6d103a6e5172fd10bbc569df
Teo Emmanuel Scarpa <46579699+ScarpaTeo@users.noreply.github.com>
2019-07-22 02:36:16 -0300

    javascript practice

---
+++ JAVASCRIPT/calculadora/calculadora.js
@@ -0,0 +1,6 @@
+function suma(){
+	var numero1=parseFloat(document.getElementById('a').value);
+	var numero2=parseFloat(document.getElementById('b').value);
+	var resul=numero1+numero2;
+	document.getElementById("resultado").value=resul;
+}
